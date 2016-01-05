0f2bd0b85379ef61b17e33a88ce9acc6819af369
Gareth Aye <gareth.aye@gmail.com>
2016-01-04 23:24:19 -0800

    Issue 67 - One 404 page to rule them all

--- src/frontend/components/NotFound.js
+++ src/frontend/components/NotFound.js
@@ -4,6 +4,19 @@ module.exports = React.createClass({
   displayName: 'NotFound',
 
   render: function() {
-    return <h1>404 Not Found</h1>;
+    return <div id="not-found">
+      <div className="container">
+        <div className="text">
+          <div className="text-header">404</div>
+          <div className="text-tagline">Oops! Where are we?</div>
+          <div className="text-body">
+            MathLeap is a feedback and assessments platform for math classes.
+            Click the button below to learn more!
+          </div>
+          <a className="button" href="https://mathleap.org">Go home</a>
+        </div>
+        <div className="mascot"></div>
+      </div>
+    </div>;
   }
 });
