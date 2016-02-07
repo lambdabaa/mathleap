e0cdcbaf5f173a5f86e30392619aaae7cb7cb2b1
gaye <gaye@mozilla.com>
2016-02-07 07:08:25 -0800

    Remove survicate js

--- public/survicate.js
+++
@@ -1,8 +0,0 @@
-(function(w) {
-  w['_sv'] = {trackingCode: 'augyDPDkMsTDzCjqVBWNXjvIMzToCiMG'};
-  var s = document.createElement('script');
-  s.src = '//api.survicate.com/assets/survicate.js';
-  s.async = true;
-  var e = document.getElementsByTagName('script')[0];
-  e.parentNode.insertBefore(s, e);
-})(window);
