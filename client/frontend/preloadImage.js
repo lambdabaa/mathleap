349ca07fd51cde4d9c53954e9393b87efe886fe8
gaye <gaye@mozilla.com>
2016-02-14 20:50:39 -0800

    Remove unused flow suppression

--- client/frontend/preloadImage.js
+++ client/frontend/preloadImage.js
@@ -2,7 +2,6 @@
 
 module.exports = async function preloadImage(url: string): Promise<void> {
   await Promise.resolve();
-  // $FlowFixMe: Why does flow not like this?
   let image = document.createElement('img');
   image.src = url;
   image.style.zIndex = '-100';
