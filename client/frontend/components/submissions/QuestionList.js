e34fd96c50f26d9bc5fd205beb0b2f079714a50e
gaye <gaye@mozilla.com>
2016-03-02 15:34:17 -0800

    Break out submission editor into separate components

---
+++ client/frontend/components/submissions/QuestionList.js
@@ -0,0 +1,38 @@
+/* @flow */
+
+let KaTeXContainer = require('../KaTeXContainer');
+let React = require('react');
+let Tabular = require('../Tabular');
+
+module.exports = function(props: Object): React.Element {
+  let {responses, num, isSubmissionPending} = props;
+  let questions = responses.map((aResponse: Object, index: number) => {
+    return [
+      <div key={`number-${index}`}
+           className="clickable-text"
+           style={{fontWeight: 'bold'}}
+           onClick={() => props.selectQuestion(index)}>
+        {index + 1}
+      </div>,
+      <KaTeXContainer key={`question-${index}`}
+                      style={{cursor: 'pointer'}}
+                      onClick={() => props.selectQuestion(index)}
+                      ascii={aResponse.question.question} />
+    ];
+  });
+
+  return <div className="submissions-edit-question-list">
+     <Tabular className="dark"
+              cols={[
+                {content: 'Questions', width: 30},
+                {content: '', width: 220}
+              ]}
+              rows={questions}
+              selected={num} />
+    {
+      isSubmissionPending ?
+        <div className="button-inverse button-disabled">Submit</div> :
+        <div className="button-inverse" onClick={props.handleSubmit}>Submit</div>
+    }
+  </div>;
+};
