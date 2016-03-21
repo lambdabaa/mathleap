387bef32b69fa8be2c1134b4c92ac91bec4f366b
gaye <gareth.aye@gmail.com>
2016-03-20 20:48:42 -0700

    Fix broken tex tests

--- test/client/backend/texify_test.js
+++ test/client/backend/texify_test.js
@@ -5,11 +5,11 @@ suite('backend/texify', () => {
   [
     ['1+1', '1+1'],
     ['2*x', '2x'],
-    ['y>2*(x+1)', 'y>2\\cdot \\left(x+1\\right)'],
+    ['y>2*(x+1)', 'y>2\\left(x+1\\right)'],
     ['(3*y)/(x/2)=0', '\\frac{\\left(3y\\right)}{\\left(\\frac{x}{2}\\right)}=0'],
-    ['3*|x+2|', '3\\cdot |x+2|'],
-    ['3x^2', '3\\cdot x^{2}'],
-    ['-2x', '-2\\cdot x']
+    ['3*|x+2|', '3|x+2|'],
+    ['3x^2', '3x^{2}'],
+    ['-2x', '-2x']
   ].forEach(testCase => {
     test(testCase[0], () => assert.equal(texify(testCase[0]), testCase[1]));
   });
