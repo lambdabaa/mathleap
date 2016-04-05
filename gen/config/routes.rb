f12bf851690c6fd748fedbe974a03b4c62af909f
gaye <gareth.aye@gmail.com>
2016-04-05 09:28:37 -0700

    Delete experimental question generation service

--- gen/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  resources :definitions
-  root 'definitions#index'
-end
--- gen/lib/predicates.pl
+++
@@ -1,8 +0,0 @@
-% Whether X is divisible by Y.
-divisible(X,Y) :-
-  0 is mod(X,Y).
-
-% Whether X is factor of Y.
-factor(X,Y) :-
-  X>1,
-  0 is mod(Y,X).
